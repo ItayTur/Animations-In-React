bb1d378ba52093f5151146a1516cdb52d3e3d6d2
ItayTur <unlessmusic1@gmail.com>
2020-04-11 20:54:49 +0300

    using css transition

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    isModalOpen: false
+  }
+
+  setIsModalOpen = isModalOpen => this.setState({ isModalOpen });
+
+  render() {
+    const { isModalOpen } = this.state;
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal isModalOpen={isModalOpen} closed={() => this.setIsModalOpen(false)}/>
+        <Backdrop isModalOpen={isModalOpen} />
+        <button className="Button" onClick={() => this.setIsModalOpen(true)}>Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
